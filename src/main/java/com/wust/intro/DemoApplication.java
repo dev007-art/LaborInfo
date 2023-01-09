037257b0db1fecaa3ea8f6fdeec16a6a5fc8fe25
mzy <307594750@qq.com>
2023-01-09 12:13:55 +0800

    #通知上传功能

--- src/main/java/com/wust/intro/DemoApplication.java
+++ src/main/java/com/wust/intro/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.wust.intro;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wust.intro.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
